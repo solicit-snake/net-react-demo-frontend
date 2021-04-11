7e8b46f0cfe19c32469b0ecb73558e146aaa2f60
Kyle Vincent <kyle.vinc11@gmail.com>
2021-04-11 23:39:28 +1000

    Init'd a few of the components required

---
+++ src/Components/Login.js
@@ -0,0 +1,14 @@
+import React, {Component, useState} from 'react';
+
+
+export class Login extends Component{
+
+
+    render(){
+        return(
+            <div className="mt-5 d-flex justify-content-left">
+                This is the login page
+            </div>
+        )
+    }
+}
---
+++ src/Components/UserInfo.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export class UserInfo extends Component{
+
+
+    render(){
+        return(
+            <div className="mt-5 d-flex justify-content-left">
+                This is the user info comp
+            </div>
+        )
+    }
+}
